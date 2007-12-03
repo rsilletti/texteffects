2fc55f8eb8d228bcee1ec3976fefd81c74a88637
rsilletti <rsilletti@a1d5ee76-e73e-0410-9c7a-c7bd858d8259>
2007-12-03 02:34:32 +0000

    Add parameters to allow list to be initially open and to set opacity values for label when collapsed.

--- trunk/jquery.listeffect.js
+++ trunk/jquery.listeffect.js
@@ -5,22 +5,52 @@
 /**
  *
 **/
-jQuery.fn.listeffect = function() {
+jQuery.fn.listeffect = function(startState , fadeStop) {
 		var noNumber = this.prev().text();
 		var thisLink = this.attr('class');
-		$("." + thisLink  + "").prev().addClass("link_" + thisLink  + "").append("(" + $("." + thisLink  + " > li").length + ")").fadeTo('fast', 0.67);
-		$("." + thisLink  + "").hide();
+			if (startState == undefined) {
+				startState = 'close';
+			}
+				if (fadeStop == undefined) {
+					if (startState.constructor == Number) {
+						fadeStop = startState;
+						startState = 'close';
+					} else {
+						fadeStop = 1;
+					}
+				}
+			if (startState == 'open') {
+				$("." + thisLink  + "").prev().addClass("link_" + thisLink  + "");
+				
+				$(".link_" + thisLink  + "").toggle(function() {
+					$("." + thisLink  + "").fadeOut('normal', function() {
+						$("." + thisLink  + "").prev().append("(" + $("." + thisLink  + " > li").length + ")");
+					});
+					$(".link_" + thisLink  + "").fadeTo('normal', fadeStop);
+				}, function() {
+					$("." + thisLink  + "").fadeIn('normal', function() {
+						$(".link_" + thisLink  + "").fadeTo('fast', 1 );
+					});
+					$("." + thisLink  + "").prev().text(noNumber);
+				});			
+			}
+		if (startState == 'close') {
+			$("." + thisLink  + "").prev().addClass("link_" + thisLink  + "").append("(" + $("." + thisLink  + " > li").length + ")").fadeTo('fast', fadeStop);
+			$("." + thisLink  + "").hide();
+			
 		$(".link_" + thisLink  + "").toggle(function() {
 			$("." + thisLink  + "").fadeIn('normal', function() {
-				$(".link_" + thisLink  + "").fadeTo('fast', 0.89);
+				$(".link_" + thisLink  + "").fadeTo('fast', 1 );
 			});
 			$("." + thisLink  + "").prev().text(noNumber);
 		}, function() {
 			$("." + thisLink  + "").fadeOut('normal', function() {
 				$("." + thisLink  + "").prev().append("(" + $("." + thisLink  + " > li").length + ")");
 			});
-			$(".link_" + thisLink  + "").fadeTo('normal', 0.66);
+			$(".link_" + thisLink  + "").fadeTo('normal', fadeStop);
 		});
+	}
+		return this;
 };
 
 })(jQuery);
