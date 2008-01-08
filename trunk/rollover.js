8f1c73e1b94599b60ca1b7775d66abcc5f12941b
rsilletti <rsilletti@a1d5ee76-e73e-0410-9c7a-c7bd858d8259>
2008-01-08 02:55:51 +0000

    Change alt attribute to name attribute for source links.

--- trunk/rollover.js
+++ trunk/rollover.js
@@ -5,22 +5,22 @@
  *   http://www.gnu.org/licenses/gpl.html
  */
 /**
- * Applies text as defined in the "alt" attribute of any source element of class "rollover_src"
+ * Applies text as defined in the "name" attribute of any source element of class "rollover_src"
  * to a single element set to html id "rollover_dest" for the duration of the hover state 
  * sustained within the source element.
  *
  * @example  destination link initial setting : <p id="rollover_dest">&nbsp;</p>
  *
- * @example  first source link : <a href="mysite.com" class="rollover_src" alt="My Site Info" title="Home">My Link</a>
+ * @example  first source link : <a href="mysite.com" class="rollover_src" name="My Site Info" title="Home">My Link</a>
  * @result  html : <p id="rollover_dest">My Site Info</p> on mouseover <p id="rollover_dest">Thanks for dropping by!</p> on mouseout.
  *
- * @example  second source link : <a href="exitlink.com" class="rollover_src" alt="Alternate Site Info" title="Aternate">Alternate Site</a>
+ * @example  second source link : <a href="exitlink.com" class="rollover_src" name="Alternate Site Info" title="Aternate">Alternate Site</a>
  * @result  html : <p id="rollover_dest">Alternate Site Info</p> on mouseover <p id="rollover_dest">Thanks for dropping by!</p> on mouseout.
 **/
 
 $(document).ready(function() {
 	$(".rollover_src").hover(function(){
-		$("#rollover_dest").html($(this).attr("alt"));
+		$("#rollover_dest").html($(this).attr("name"));
 		},function(){
 		$("#rollover_dest").html("Thanks for dropping by!");
 	});
--- trunk/rolloverj.js
+++ trunk/rolloverj.js
@@ -5,22 +5,22 @@
  *   http://www.gnu.org/licenses/gpl.html
  */
 /**
- * Applies text as defined in the "alt" attribute of any source element of class "rollover_src"
+ * Applies text as defined in the "name" attribute of any source element of class "rollover_src"
  * to a single element set to html id "rollover_dest" for the duration of the hover state 
  * sustained within the source element.
  *
  * @example  destination link initial setting : <p id="rollover_dest">&nbsp;</p>
  *
- * @example  first source link : <a href="mysite.com" class="rollover_src" alt="My Site Info" title="Home">My Link</a>
+ * @example  first source link : <a href="mysite.com" class="rollover_src" name="My Site Info" title="Home">My Link</a>
  * @result  html : <p id="rollover_dest">My Site Info</p> on mouseover <p id="rollover_dest">Thanks for dropping by!</p> on mouseout.
  *
- * @example  second source link : <a href="exitlink.com" class="rollover_src" alt="Alternate Site Info" title="Aternate">Alternate Site</a>
+ * @example  second source link : <a href="exitlink.com" class="rollover_src" name="Alternate Site Info" title="Aternate">Alternate Site</a>
  * @result  html : <p id="rollover_dest">Alternate Site Info</p> on mouseover <p id="rollover_dest">Thanks for dropping by!</p> on mouseout.
 **/
 jQuery.noConflict();
 jQuery(document).ready(function() {
 jQuery(".rollover_src").hover(function(){
-jQuery("#rollover_dest").html(jQuery(this).attr("alt"));
+jQuery("#rollover_dest").html(jQuery(this).attr("name"));
 		},function(){
 jQuery("#rollover_dest").html("Thanks for dropping by!");
 	});
