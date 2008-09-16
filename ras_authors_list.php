95f0958ed1b65095a26a96282b334edf4922155e
rsilletti <rsilletti@a1d5ee76-e73e-0410-9c7a-c7bd858d8259>
2008-09-16 23:22:30 +0000

    Add mod_authors_list and associated functions to support 4.0.7 plugin.

--- ras_authors_list.php
+++ ras_authors_list.php
@@ -56,7 +56,7 @@ function ras_authors_list ($atts)
 		return '';
 }
 
-//------------------------------------- 4.0.7 model core modification required -----------------------------------------
+//------------------------------------- 4.0.7 model, use mod_author in place of core author tag --------------
 
 function mod_authors_list ($atts, $thing = NULL) 
 {
@@ -121,8 +121,8 @@ function mod_authors_list ($atts, $thing = NULL)
 				
 				if(in_array($author_priv , $privs))
 				{
-						$author_name = safe_field('RealName', 'txp_users', $where);
-			            $author_id = safe_field('name', 'txp_users', $where);
+						$author_name = safe_field('RealName', 'txp_users', $where); // A bit clunky
+					    $author_id = safe_field('name', 'txp_users', $where);
 						$thisauthor = array('realname' => $author_name, 'name' => $author_id);
 						$thisauthor['is_first'] = ($count == 1);
 						$thisauthor['is_last'] = ($count == $last);
@@ -141,5 +141,114 @@ function mod_authors_list ($atts, $thing = NULL)
 
 		return '';
 }
+// -------------------------------------------------------------
+
+	function mod_author($atts)
+	{
+		global $thisarticle, $s, $thisauthor;
+
+		assert_article();
+
+		extract(lAtts(array(
+			'link'         => '',
+			'section'      => '',
+			'this_section' => 0,
+		), $atts));
+
+		$author_name = get_author_name($thisarticle['authorid']);
+		
+		if (!empty($thisauthor['realname']))
+		{
+			$author_name = $thisauthor['realname'];
+		}
+
+		$section = ($this_section) ? ( $s == 'default' ? '' : $s ) : $section;
+
+		return ($link) ?
+			href($author_name, pagelinkurl(array('s' => $section, 'author' => $author_name))) :
+			$author_name;
+	}
+// -------------------------------------------------------------
+	function if_first_author($atts, $thing)
+	{
+		global $thisauthor;
+		assert_author();
+		return parse(EvalElse($thing, !empty($thisauthor['is_first'])));
+	}
+
+// -------------------------------------------------------------
+	function if_last_author($atts, $thing)
+	{
+		global $thisauthor;
+		assert_author();
+		return parse(EvalElse($thing, !empty($thisauthor['is_last'])));
+	}
+//--------------------------------------------------------------
+  	function assert_author() {
+         global $thisauthor;
+		    if (empty($thisauthor))
+              trigger_error(gTxt('error_author_context'));
+      }
+
+//--------------------------------------------------------------------------------
+// Array values called by array name as text (no leading $) and elemnet as text --
+//--------------------------------------------------------------------------------
+// Attributes are array_name & element for self closing tags ---------------------
+
+    function ras_thing($atts, $thing = NULL)  { // The tag definition
+	 
+	 		extract(lAtts(array(
+			'array_name'        => NULL,
+			'element'           => NULL,
+		), $atts));
+		
+		  $thing = trim('ras_'.$thing);
+
+		  $array_name = 'ras_'.$array_name ; 		 
+		  
+		 if(is_callable($thing))		   
+		  {
+		  	 return $thing();			 
+		  }
+		  
+		if(is_callable($array_name))		   
+		  {
+		  	 return $array_name($element);			 
+		  }
+		  
+		 return ''; 
+ 	}
+//---------------------------------------------------------------------------------
+	
+	function ras_thisauthor($element)  { 
+		global $thisauthor;
+		
+		$available_elements = array(
+			'realname',
+			'name'
+		);
+		
+		if (!in_array($element, $available_elements))
+		{
+			return ' element not an array member ';
+			
+		} else {
+		 
+			return $thisauthor[$element]; 
+		}	
+	}
+//--------------------------------------------------------------------------------
+// Functions list, to add your own and make thier value accessable via ras_thing, 
+// alias it by prefixing ras_ to the variable name as the function name. 
+// Render your variable global and return it as the function return value.
+//------------------------------- Thus -------------------------------------------
+
+	
+	function ras_author() {
+		global $author;
+		return $author;
+	}
+	
+// Tags for variables are wraptags of the variable name as text  //
 
 ?>
