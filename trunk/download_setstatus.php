18ef34dcf52224fdd3b5ee63ce009722b130509a
rsilletti <rsilletti@a1d5ee76-e73e-0410-9c7a-c7bd858d8259>
2009-11-20 21:11:33 +0000

    Restrict status options for atts in download_setstatus

--- trunk/download_setstatus.php
+++ trunk/download_setstatus.php
@@ -1,16 +1,20 @@
 <?php
-	function download_setstatus( $atts ) 
-	{
-	
-		global $thisfile;
-		assert_file();
+
+function ras_download_setstatus( $atts ) 
+{	
+	global $thisfile;
+	assert_file();
 				
 		extract(lAtts(array(
 			'setstatus' => '4',
 		), $atts));
-		
-		$where = "`id`='".$thisfile['id']."'";
-		$set = "`status`='".$setstatus."'";
-			safe_update('txp_file', $set, $where );
-	}
+	if( $setstatus == 2 || $setstatus == 3 || $setstatus == 4 ) {	
+	$where = "`id`='".$thisfile['id']."'";
+	$set = "`status`='".$setstatus."'";
+		safe_update('txp_file', $set, $where );
+        } else {
+     return "Status must be set to 2, 3 or 4 ";
+     }
+}
+
 ?>
