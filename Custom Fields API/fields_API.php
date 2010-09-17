a48a072a051b6b9280b42305ed037bc1eed8f4ba
rsilletti <rsilletti@a1d5ee76-e73e-0410-9c7a-c7bd858d8259>
2010-09-17 07:19:07 +0000

    Terms of use.

--- Custom Fields API/fields_API.php
+++ Custom Fields API/fields_API.php
@@ -1,12 +1,32 @@
 <?php
 /**
-* @license http://www.gnu.org/licenses/licenses.html#GPL
+* Returns custom field names by textpattern table column relationship to name set in preferences.
+*
+* Copyright (C) 2010 Rick Silletti
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+* 02110-1301 USA
+*
+* rsilletti@gmail.com
+*
+* Rick Silletti, 1 April 1990
+*
 * @package txpcustom_fields
 */
 
 /**
-* Returns custom field names by textpattern table column relationship to name set in preferences.
-* 
 *
 * @package txpcustom_fields
 * @author Rick Silletti
@@ -18,7 +38,7 @@
 * and returns the textpattern table column name that
 * is associated with it.
 * @param text custom field name.
-* @return text textpattern column name. 
+* @return text $name textpattern column name. 
 */
 
 class FieldByName {
