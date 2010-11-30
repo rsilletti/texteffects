a4be127dd2abf54c9d9058ffecb271ce81e312b7
rsilletti <rsilletti@a1d5ee76-e73e-0410-9c7a-c7bd858d8259>
2010-11-30 00:02:30 +0000

    Add Docs and spruce up formatting.

--- trunk/Custom Fields API/ras_level_stats.php
+++ trunk/Custom Fields API/ras_level_stats.php
@@ -1,22 +1,29 @@
 <?php
 
-function ras_level_stats($atts) {
+/**
+ * Render tabular statistics including total and sorted by level of difficulty.
+ *
+ * @param	string	$text	Custom field name.
+ * @return	string	HTML
+ */
+ 
+	function ras_level_stats($atts) {
 		extract(lAtts(array(
 			'fieldname' => '',
 		), $atts));
-		
- 	$totals = new AggregateByName($fieldname);
- 	$stats = $totals->articlesData("Excerpt,".$totals->field."");
- 	$sum = $totals->sumName();
- 	
- 				$es = 0;
+
+	$totals = new AggregateByName($fieldname);
+	$stats = $totals->articlesData("Excerpt,".$totals->field."");
+	$sum = $totals->sumName();
+ 
+				$es = 0;
 				$mod = 0;
 				$diff = 0;
-				
+
 			foreach($stats as $elem)
 				{
 				$elem['Excerpt'] = strtolower($elem['Excerpt']);
-				
+
 				switch($elem['Excerpt'])
 					{
 					case 'easy' : $es += $elem[$totals->field]; break;
@@ -24,12 +31,12 @@ function ras_level_stats($atts) {
 					case 'difficult' : $diff += $elem[$totals->field]; break;
 					}
 				}
-				
-				$rs = '<table cellpadding="4" cellspacing="10" border="1" style="font-size:11px;"><tr><td>Easy</td><td>'.$es.'</td></tr>'.n.
-					  '<tr><td>Moderate</td><td> '.$mod.'</td></tr>'.n.
-					  '<tr><td>Difficult</td><td>'.$diff.'</td></tr>'.n.
-					  '<tr><td>Total</td><td>'.$sum.'</td></tr></table>';					  
-				return $rs;
+
+		$rs =	'<table cellpadding="4" cellspacing="10" border="1" style="font-size:11px;"><tr><td>Easy</td><td>'.$es.'</td></tr>'.n.
+				'<tr><td>Moderate</td><td> '.$mod.'</td></tr>'.n.
+				'<tr><td>Difficult</td><td>'.$diff.'</td></tr>'.n.
+				'<tr><td>Total</td><td>'.$sum.'</td></tr></table>';
+	return $rs;
 }
 
 ?>
