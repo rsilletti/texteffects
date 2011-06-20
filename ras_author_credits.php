1abec440286ec99d7d48b0343d5d80b5e8e86a60
rsilletti <rsilletti@a1d5ee76-e73e-0410-9c7a-c7bd858d8259>
2011-06-20 19:57:22 +0000

    Refactored ras_author_credits

--- ras_author_credits.php
+++ ras_author_credits.php
@@ -21,8 +21,8 @@
 *
 */
 
-function ras_author_credits ($atts, $thing = NULL) 
-{
+	function ras_author_credits ($atts, $thing = NULL) 
+	{
 	global $s, $thisauthor;
 		extract(lAtts(array(
 			'break'        => '',
@@ -83,48 +83,9 @@ function ras_author_credits ($atts, $thing = NULL)
 			if (empty($form) && empty($thing))
 			{
 				$where_author = "AuthorID='".$row['name']."'";
-				$where_content = "author='".$row['name']."'";				
-				$select = 0;
-
-				switch($select_by)
-				{
-					case 0 : break;			
-						// 1 true if an article has been posted
-					case 1 : $select = safe_count('textpattern', $where_author); break;
-						// 2 true if a file has been uploaded
-					case 2 : $select = safe_count('txp_file', $where_content); break;
-						// 3 true if an article has been posted or a file uploaded
-					case 3 : $select = (safe_count('txp_file', $where_content) || safe_count('textpattern', $where_author)) ; break;
-						// 4 true if an article has been posted and a file uploaded
-					case 4 : $select = (safe_count('txp_file', $where_content) && safe_count('textpattern', $where_author)) ; break;
-						// 5 true if an article has been posted and a file has not been uploaded
-					case 5 : $select = (!safe_count('txp_file', $where_content) && safe_count('textpattern', $where_author)) ; break;
-						// 6 true if a file has been uploaded and an article has not been posted
-					case 6 : $select = (safe_count('txp_file', $where_content) && !safe_count('textpattern', $where_author)) ; break;
-						// 7 true if an image has been uploaded
-					case 7 : $select = safe_count('txp_image', $where_content); break;
-						// 8 true if an image or a file has been uploaded
-					case 8 : $select = (safe_count('txp_file', $where_content) || safe_count('txp_image', $where_content)) ; break;
-						// 9 true if an image and a file have been uploaded
-					case 9 : $select = (safe_count('txp_file', $where_content) && safe_count('txp_image', $where_content)) ; break;
-						// 10 true if an image has been uploaded and a file has not been uploaded
-					case 10 : $select = (!safe_count('txp_file', $where_content) && safe_count('txp_image', $where_content)) ; break;
-						// 11 true if a file has been uploaded and an image has not been uploaded
-					case 11 : $select = (safe_count('txp_file', $where_content) && !safe_count('txp_image', $where_content)) ; break;
-						// 12 true if an article has been posted or an image uploaded
-					case 12 : $select = (safe_count('txp_image', $where_content) || safe_count('textpattern', $where_author)) ; break;
-						// 13 true if an article has been posted and an image uploaded
-					case 13 : $select = (safe_count('txp_image', $where_content) && safe_count('textpattern', $where_author)) ; break;
-						// 14 true if an article has been posted and an image has not been uploaded
-					case 14 : $select = (!safe_count('txp_image', $where_content) && safe_count('textpattern', $where_author)) ; break;
-						// 15 true if an article has not been posted and an image has been uploaded
-					case 15 : $select = (safe_count('txp_image', $where_content) && !safe_count('textpattern', $where_author)) ; break;
-						// 16 true if an article has been posted or an image uploaded or a file uploaded
-					case 16 : $select = (safe_count('txp_image', $where_content) || safe_count('txp_file', $where_content) || safe_count('textpattern', $where_author)) ; break;
-						// 17 true if an article has been posted and an image uploaded and a file uploaded
-					case 17 : $select = (safe_count('txp_image', $where_content) && safe_count('txp_file', $where_content) && safe_count('textpattern', $where_author)) ; break;
-					default : $select = 0;
-				}
+				$where_content = "author='".$row['name']."'";	
+				
+			$select = ras_select_by($select_by, $where_author, $where_content);
 
 			if($select || !$active_only) 
 			{
@@ -143,7 +104,7 @@ function ras_author_credits ($atts, $thing = NULL)
 							case 'files'    : $option[] = ras_filecount(); break;
 							case 'images'   : $option[] = ras_imagecount(); break;
 						}
-				} (count(do_list($rank_by)) == 1) ? $option[] = NULL : '';
+					} (count(do_list($rank_by)) == 1) ? $option[] = NULL : '';
 
 				$data[] = array('firstcount' => $option['0'] , 'nextcount' => $option['1'], 'thehtml' => ($link) ? href($author_name, pagelinkurl(array('s' => $section, 'author' => $author_name))) : $author_name);
 				}
@@ -153,47 +114,8 @@ function ras_author_credits ($atts, $thing = NULL)
 		{
 				$where_author = "AuthorID='".$row['name']."'";
 				$where_content = "author='".$row['name']."'";
-				$select = 0;
 
-				switch($select_by)
-				{
-					case 0 : break;			
-						// 1 true if an article has been posted
-					case 1 : $select = safe_count('textpattern', $where_author); break;
-						// 2 true if a file has been uploaded
-					case 2 : $select = safe_count('txp_file', $where_content); break;
-						// 3 true if an article has been posted or a file uploaded
-					case 3 : $select = (safe_count('txp_file', $where_content) || safe_count('textpattern', $where_author)) ; break;
-						// 4 true if an article has been posted and a file uploaded
-					case 4 : $select = (safe_count('txp_file', $where_content) && safe_count('textpattern', $where_author)) ; break;
-						// 5 true if an article has been posted and a file has not been uploaded
-					case 5 : $select = (!safe_count('txp_file', $where_content) && safe_count('textpattern', $where_author)) ; break;
-						// 6 true if a file has been uploaded and an article has not been posted
-					case 6 : $select = (safe_count('txp_file', $where_content) && !safe_count('textpattern', $where_author)) ; break;
-						// 7 true if an image has been uploaded
-					case 7 : $select = safe_count('txp_image', $where_content); break;
-						// 8 true if an image or a file has been uploaded
-					case 8 : $select = (safe_count('txp_file', $where_content) || safe_count('txp_image', $where_content)) ; break;
-						// 9 true if an image and a file have been uploaded
-					case 9 : $select = (safe_count('txp_file', $where_content) && safe_count('txp_image', $where_content)) ; break;
-						// 10 true if an image has been uploaded and a file has not been uploaded
-					case 10 : $select = (!safe_count('txp_file', $where_content) && safe_count('txp_image', $where_content)) ; break;
-						// 11 true if a file has been uploaded and an image has not been uploaded
-					case 11 : $select = (safe_count('txp_file', $where_content) && !safe_count('txp_image', $where_content)) ; break;
-						// 12 true if an article has been posted or an image uploaded
-					case 12 : $select = (safe_count('txp_image', $where_content) || safe_count('textpattern', $where_author)) ; break;
-						// 13 true if an article has been posted and an image uploaded
-					case 13 : $select = (safe_count('txp_image', $where_content) && safe_count('textpattern', $where_author)) ; break;
-						// 14 true if an article has been posted and an image has not been uploaded
-					case 14 : $select = (!safe_count('txp_image', $where_content) && safe_count('textpattern', $where_author)) ; break;
-						// 15 true if an article has not been posted and an image has been uploaded
-					case 15 : $select = (safe_count('txp_image', $where_content) && !safe_count('textpattern', $where_author)) ; break;
-						// 16 true if an article has been posted or an image uploaded or a file uploaded
-					case 16 : $select = (safe_count('txp_image', $where_content) || safe_count('txp_file', $where_content) || safe_count('textpattern', $where_author)) ; break;
-						// 17 true if an article has been posted and an image uploaded and a file uploaded
-					case 17 : $select = (safe_count('txp_image', $where_content) && safe_count('txp_file', $where_content) && safe_count('textpattern', $where_author)) ; break;
-					default : $select = 0;
-				}
+			$select = ras_select_by($select_by, $where_author, $where_content);
 
 			if($select || !$active_only) 
 			{
@@ -226,6 +148,7 @@ function ras_author_credits ($atts, $thing = NULL)
 			$nextcount[$key] = $row['nextcount'];
 			$thehtml[$key] = $row['thehtml'];
 		}
+		
 		($option['1'] == NULL) ?
 		array_multisort($firstcount, SORT_DESC, $thehtml, SORT_ASC, $data) :
 		array_multisort($firstcount, SORT_DESC, $nextcount, SORT_DESC, $data);
@@ -241,13 +164,58 @@ function ras_author_credits ($atts, $thing = NULL)
 			}
 
 	return '';
-}
+	}
+// -------------------------------------------------------------
+
+	function ras_select_by($selection, $where_author, $where_content )
+	{
+		$select = 0;
+			switch($selection)
+				{
+					case 0 : break;			
+						// 1 true if an article has been posted
+					case 1 : $select = safe_count('textpattern', $where_author); break;
+						// 2 true if a file has been uploaded
+					case 2 : $select = safe_count('txp_file', $where_content); break;
+						// 3 true if an article has been posted or a file uploaded
+					case 3 : $select = (safe_count('txp_file', $where_content) || safe_count('textpattern', $where_author)) ; break;
+						// 4 true if an article has been posted and a file uploaded
+					case 4 : $select = (safe_count('txp_file', $where_content) && safe_count('textpattern', $where_author)) ; break;
+						// 5 true if an article has been posted and a file has not been uploaded
+					case 5 : $select = (!safe_count('txp_file', $where_content) && safe_count('textpattern', $where_author)) ; break;
+						// 6 true if a file has been uploaded and an article has not been posted
+					case 6 : $select = (safe_count('txp_file', $where_content) && !safe_count('textpattern', $where_author)) ; break;
+						// 7 true if an image has been uploaded
+					case 7 : $select = safe_count('txp_image', $where_content); break;
+						// 8 true if an image or a file has been uploaded
+					case 8 : $select = (safe_count('txp_file', $where_content) || safe_count('txp_image', $where_content)) ; break;
+						// 9 true if an image and a file have been uploaded
+					case 9 : $select = (safe_count('txp_file', $where_content) && safe_count('txp_image', $where_content)) ; break;
+						// 10 true if an image has been uploaded and a file has not been uploaded
+					case 10 : $select = (!safe_count('txp_file', $where_content) && safe_count('txp_image', $where_content)) ; break;
+						// 11 true if a file has been uploaded and an image has not been uploaded
+					case 11 : $select = (safe_count('txp_file', $where_content) && !safe_count('txp_image', $where_content)) ; break;
+						// 12 true if an article has been posted or an image uploaded
+					case 12 : $select = (safe_count('txp_image', $where_content) || safe_count('textpattern', $where_author)) ; break;
+						// 13 true if an article has been posted and an image uploaded
+					case 13 : $select = (safe_count('txp_image', $where_content) && safe_count('textpattern', $where_author)) ; break;
+						// 14 true if an article has been posted and an image has not been uploaded
+					case 14 : $select = (!safe_count('txp_image', $where_content) && safe_count('textpattern', $where_author)) ; break;
+						// 15 true if an article has not been posted and an image has been uploaded
+					case 15 : $select = (safe_count('txp_image', $where_content) && !safe_count('textpattern', $where_author)) ; break;
+						// 16 true if an article has been posted or an image uploaded or a file uploaded
+					case 16 : $select = (safe_count('txp_image', $where_content) || safe_count('txp_file', $where_content) || safe_count('textpattern', $where_author)) ; break;
+						// 17 true if an article has been posted and an image uploaded and a file uploaded
+					case 17 : $select = (safe_count('txp_image', $where_content) && safe_count('txp_file', $where_content) && safe_count('textpattern', $where_author)) ; break;
+				}
+	return $select;
+	}
 
 // -------------------------------------------------------------
 
-function ras_author($atts)
-{
-	global $thisarticle, $s, $thisauthor;
+	function ras_author($atts)
+	{
+		global $thisarticle, $s, $thisauthor;
 
 		extract(lAtts(array(
 			'link'         => '',
@@ -255,78 +223,99 @@ function ras_author($atts)
 			'this_section' => 0,
 		), $atts));
 
-		$author_name = get_author_name($thisarticle['authorid']);		
+		$author_name = get_author_name($thisarticle['authorid']);
+
 		if (!empty($thisauthor['realname']))
 		{
 			$author_name = $thisauthor['realname'];
 		}
+
 		$section = ($this_section) ? ( $s == 'default' ? '' : $s ) : $section;
-	return ($link) ? href($author_name, pagelinkurl(array('s' => $section, 'author' => $author_name)), ' rel="author"') : $author_name;
-}
+
+		return ($link) ?
+			href($author_name, pagelinkurl(array('s' => $section, 'author' => $author_name)), ' rel="author"') : $author_name;
+	}
 
 // -------------------------------------------------------------
 
-function ras_user()
-{
-	global $thisarticle,  $thisauthor, $author ;
-		$author_name = $author;
-		$login_name = $thisarticle['authorid']; 
+	function ras_user()
+	{
+		global $thisarticle,  $thisauthor, $author ;
 
+		$author_name = $author;
+		$login_name = $thisarticle['authorid']; //?
+		
 		if (!empty($thisauthor['name']))
 		{
 			$author_name = $thisauthor['name'];
 		}
-	return $author_name;
-}
+
+		return $author_name;
+	}
 // -------------------------------------------------------------
 
-function ras_filecount()
-{
-	global $thisauthor;	
-		ras_assert_author();	
-		$where = "author='".$thisauthor['name']."'";
-		$rs = safe_row('COUNT(id)' , 'txp_file' , $where);	
-	return $rs['COUNT(id)'];   
-}
+	function ras_filecount()
+	{
+	global $thisauthor;
+	
+	ras_assert_author();
+	
+	$where = "author='".$thisauthor['name']."'";
+	$rs = safe_row('COUNT(id)' , 'txp_file' , $where);
+	
+	return $rs['COUNT(id)'];
+
+	}
 
 // -------------------------------------------------------------
-function ras_articlecount()
-{
-	global $thisauthor;	
-		ras_assert_author();	
-		$where = "AuthorID='".$thisauthor['name']."'";
-		$rs = safe_row('COUNT(ID)' , 'textpattern' , $where);
+	function ras_articlecount()
+	{
+	global $thisauthor;
+	
+	ras_assert_author();
+	
+	$where = "AuthorID='".$thisauthor['name']."'";
+	$rs = safe_row('COUNT(ID)' , 'textpattern' , $where);
+	
 	return $rs['COUNT(ID)'];
-}
-
+	}
+	
 // -------------------------------------------------------------
 
-function ras_imagecount()
-{
+	function ras_imagecount()
+	{
 	global $thisauthor;
-    	ras_assert_author();               	
-		where = "author='".$thisauthor['name']."'";
-		$rs = safe_row('COUNT(id)' , 'txp_image' , $where);	
-	return $rs['COUNT(id)'];
-}
 
+	ras_assert_author();
+
+	$where = "author='".$thisauthor['name']."'";
+	$rs = safe_row('COUNT(id)' , 'txp_image' , $where);
+	
+	return $rs['COUNT(id)'];
+	}
+	
 // -------------------------------------------------------------
 
-function ras_linkcount()
-{
+	function ras_linkcount()
+	{
 	global $thisauthor;
-		ras_assert_author();	
-		$where = "author='".$thisauthor['name']."'";
-		$rs = safe_row('COUNT(id)' , 'txp_link' , $where);	
+	
+	ras_assert_author();
+	
+	$where = "author='".$thisauthor['name']."'";
+	$rs = safe_row('COUNT(id)' , 'txp_link' , $where);
+	
 	return $rs['COUNT(id)'];
-}
-
+	}
+	
 
 //--------------------------------------------------------------
-function ras_assert_author() {
+	function ras_assert_author() {
 	global $thisauthor;
-	if (empty($thisauthor))
-	trigger_error(gTxt('error_author_context'));
-}
+	if(empty($thisauthor))
+		trigger_error(gTxt('error_author_context'));
+	}
+
+
 
 ?>
